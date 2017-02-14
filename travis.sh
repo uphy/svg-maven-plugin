a403e0e6b07db7541302752050b39d3944199e3d
Karsten Kraus <Karsten.Kraus@swr.de>
2017-02-14 19:03:47 +0100

    travis debugging

--- travis.sh
+++ travis.sh
@@ -5,6 +5,11 @@ if [ "$TRAVIS_TAG" != "" ]; then
 fi
 
 ./mvnw install -B
+echo ${TEST_VAR}
+echo ${TEST_VAR_SECURE}
+if [ "$TEST_VAR_SECURE" != "" ]; then
+    echo "hmmja..."
+fi
 
 if [ "$TRAVIS_TAG" != "" ]; then
     ./mvnw deploy -Dmaven.javadoc.skip=true -Dgithub.global.userName=${GITHUB_USER_NAME} -Dgithub.global.password=${GITHUB_AUTH_TOKEN} -B
